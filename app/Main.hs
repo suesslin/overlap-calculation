6c944c8eedcf0230d238c5b8a382cc45c66d5fc7
Lou SÜSSLIN <suesslin@pm.me>
2024-10-02 02:53:53 +0200

    Try average area of quad tree tiles to tell concentration

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ import qualified Data.ByteString.Lazy as B
 import Data.List (isSuffixOf)
 import System.Directory
 
-import Data.QuadTree (makeTree, setLocation, fuseTree, tile)
+import Data.QuadTree 
 
 data LayoutType = Baseline | RV | RH deriving (Show, Eq)
 instance FromJSON LayoutType where
@@ -104,21 +104,59 @@ main = do
     
     let interviews = [i | Right i <- maybeInterviews]
 
-    let filterLayout = Baseline
-    let filterTracesCondition = WithoutTraces
-
     -- All touch points for one given condition
-    let interviewsOfOneCondition = filter (\i -> layoutType (condition i) == filterLayout && tracesDisplayingCondition (condition i) == filterTracesCondition) interviews
-   
-    let allTouchPointCoords = concatMap (map coordinate . touchPoints) interviewsOfOneCondition
+    let interviewsOfOneCondition filterLayout filterTracesCondition = filter (\i -> layoutType (condition i) == filterLayout && tracesDisplayingCondition (condition i) == filterTracesCondition) interviews
+    let touchPointsForLayout filterLayout filterTracesCondition = concatMap (map coordinate . touchPoints) (interviewsOfOneCondition filterLayout filterTracesCondition)
 
-    -- mapM_ putStrLn $ map (\(Coordinate coord) -> show coord) allTouchPointCoords
+    -- Touch points for layouts
+    let tpsBnoTr = touchPointsForLayout Baseline WithoutTraces
+    let tpsRhNoTr = touchPointsForLayout RH WithoutTraces
+    let tpsRvNoTr = touchPointsForLayout RV WithoutTraces
+    
+    -- Set of touch points for layouts without traces
+    let tpsBnoTr = concatMap (map coordinate . touchPoints) interviewsForCondition where interviewsForCondition = interviewsOfOneCondition Baseline WithoutTraces
+    
+    -- Set of touch points for static UI
+    -- Copied thrice to ensure equal amount of users as for the three layouts that simulate the adaptive UI
+    let tpsStaticNoTr = concat $ replicate 3 tpsBnoTr
 
-    let tree = foldl (\t (Coordinate coord) -> setLocation coord True t) (makeTree (1920,1080) False) allTouchPointCoords
-    let fusedTree = fuseTree tree
+    -- Set of touch points for adaptive UI (without traces)
+    -- Simulated by taking all tps over all uis (without traces)
+    let tpsAdaptiveNoTr = tpsBnoTr ++ tpsRhNoTr ++ tpsRvNoTr
 
-    mapM_ print $ tile fusedTree
+    -- Create tree 
+    let tree tps = foldl (\t (Coordinate coord) -> setLocation coord True t) (makeTree (1920,1080) False) tps
+
+    let staticTree = tree tpsStaticNoTr
+    let adaptiveTree = tree tpsAdaptiveNoTr
+
+
+    -- Breaks down into tiles
+    -- Tiles are of type (a, Region), or (a, (Int, Int, Int, Int))
+    -- where the region states x_min, y_min, x_max, y_max
+    -- Meaning: The smaller the tile, the more points are accumulated in this small area
+    let tiles tr = tile tr
+
+    -- Calculation idea:
+    -- Subregions become smaller 
+    -- (smaller distance between xMax and and xMin and between yMax and yMin)
+    -- if touchpoints are concentrated in this area.
+    -- This concentration can be used a simplified indicator that there may be 
+    -- more overlap (Though it is an approximation, not perfectly the same as overlap).
+    -- We may therefore calculate the average area for each tile in the trees for each touch point data set.
+    -- Area: (xMax-xMin) * (yMax-yMin) 
+    --       regionArea exists in QuadTree library
+    let averageArea tls = fromIntegral (sum (map (regionArea . snd) tls)) / fromIntegral (length tls)
+    
+    print . averageArea . tiles $ staticTree
+    print . averageArea . tiles $ adaptiveTree
 
 decodeInterview :: FilePath -> IO (Either String Interview)
 decodeInterview path = do
     return . eitherDecode =<< B.readFile path 
+
+-- regionDepth :: Region -> Int
+-- regionDepth (xMin, yMin, xMax, yMax) 
+--     = .
+--         where width = xMax - xMin
+--               height = yMax - yMin
